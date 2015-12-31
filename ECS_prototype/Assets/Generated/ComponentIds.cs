dbdb032c93cac2b75ef9d66c315f880d8cb3ce86
Mike Ton <mike.ton@gmail.com>
2015-12-30 21:54:03 -0600

    Adding View, Resource Component.  Setting up AddViewSystem.

--- ECS_prototype/Assets/Generated/ComponentIds.cs
+++ ECS_prototype/Assets/Generated/ComponentIds.cs
@@ -2,18 +2,24 @@ public static class ComponentIds {
     public const int InputPress = 0;
     public const int InputRelease = 1;
     public const int Position = 2;
+    public const int Resource = 3;
+    public const int View = 4;
 
-    public const int TotalComponents = 3;
+    public const int TotalComponents = 5;
 
     public static readonly string[] componentNames = {
         "InputPress",
         "InputRelease",
-        "Position"
+        "Position",
+        "Resource",
+        "View"
     };
 
     public static readonly System.Type[] componentTypes = {
         typeof(InputPressComponent),
         typeof(InputReleaseComponent),
-        typeof(PositionComponent)
+        typeof(PositionComponent),
+        typeof(ResourceComponent),
+        typeof(ViewComponent)
     };
 }
---
+++ ECS_prototype/Assets/Platformer2D/Code/Features/RenderResource/AddViewSystem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Entitas;
+
+public class AddViewSystem : IReactiveSystem {
+
+	readonly Transform _viewContainer = new GameObject("View").transform; // name of transform parent of new View Object
+	
+	#region IReactiveExecuteSystem implementation
+	public void Execute (List<Entity> entities){
+		Debug.LogFormat("Add View : {0} ", entities);
+	}
+
+	public TriggerOnEvent trigger {
+		get {
+			return Matcher.Resource.OnEntityAdded();
+		}
+	}
+	#endregion
+
+
+}
