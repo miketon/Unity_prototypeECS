64a2def8de047b59ba272db9a21a046602e3423d
Mike Ton <mike.ton@gmail.com>
2015-12-30 19:45:40 -0600

    ECS InputPressSystem.cs

---
+++ ECS_prototype/Assets/Platformer2D/Code/Features/Input/InputPressSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Entitas;
+
+public class InputPressSystem : IReactiveSystem, ISetPool {
+	
+	private Pool _pool;
+
+	#region IReactiveExecuteSystem implementation
+
+	public void Execute (List<Entity> entities){
+		Debug.LogFormat("Key Pressed : {0} ", KeyCode.Space)	;
+		var inputEntity = entities.SingleEntity() ;
+		_pool.DestroyEntity(inputEntity)          ; // destroy  input object
+	}
+
+	public TriggerOnEvent trigger {
+		get {
+			return Matcher.Input.OnEntityAdded();
+		}
+	}
+
+	#endregion
+
+	
+	#region ISetPool implementation
+
+	public void SetPool (Pool pool){
+		_pool = pool;
+	}
+
+	#endregion
+}
--- ECS_prototype/Assets/Platformer2D/Code/Scripts/gameController.cs
+++ ECS_prototype/Assets/Platformer2D/Code/Scripts/gameController.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Entitas;
+using Entitas.Unity.VisualDebugging;
 
 public class gameController : MonoBehaviour {
 
-	private Entity _e;
+	private Entity  _e;
+	private Systems _systems;
 
 	// Use this for initialization
 	void Start () {
@@ -13,17 +15,34 @@ public class gameController : MonoBehaviour {
 //		pool.GetGroup(Matcher.AllOf(Matcher.Position)); // what does this do???
 		_e = pool.CreateEntity();
 		_e.AddPosition(0.0f, 3.0f, 1.9f);
+
+		_systems = createSystems(Pools.pool);
+		_systems.Initialize();
+
 	}
 
 	void Update(){
 		if(Input.GetKeyDown(KeyCode.Space)){
-			Debug.LogFormat("Key Pressed : {0} ", KeyCode.Space)	;
 			_e.ReplacePosition(0.0f, 0.0f, 0.0f);
 		}
 		else if(Input.GetKeyUp(KeyCode.Space)){
 			Debug.LogFormat("Key Released : {0} ", KeyCode.Space)	;
 			_e.ReplacePosition(1.0f, 1.0f, 1.0f);
 		}
+
+		_systems.Execute();
+	}
+
+	Systems createSystems(Pool pool){
+		#if (UNITY_EDITOR)
+		return new DebugSystems()
+		#else
+		return new Systems()
+		#endif	
+
+		// Input
+		.Add(pool.CreateSystem<InputPressSystem>());
+		
 	}
 	
 }
